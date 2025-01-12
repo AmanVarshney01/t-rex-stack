d49d6b7da7ab55c6167cefd82cd4dea76ae8200d
Aman Varshney <av.amanvarshney11@gmail.com>
2025-01-12 12:12:37 +0530

    add header in dashboard layout

--- client/app/routes/dashboard.tsx
+++ client/app/routes/dashboard.tsx
@@ -1,7 +1,18 @@
+import { Book } from "lucide-react";
+import { Outlet } from "react-router";
+
 export default function Dashboard() {
   return (
-    <div className="flex h-svh items-center justify-center">
-      <h1>Dashboard</h1>
+    <div className="grid h-svh grid-rows-[auto_1fr]">
+      <header className="flex flex-row p-4">
+        <div className="flex flex-row gap-2">
+          <Book />
+          <h1>BookMate</h1>
+        </div>
+      </header>
+      <div className="size-full p-4">
+        <Outlet />
+      </div>
     </div>
   );
 }
